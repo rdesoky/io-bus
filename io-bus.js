6b387d9c72f00ad40898bb3293f9605a7334cc53
Ramy Eldesoky <rdesoky@gmail.com>
2015-12-16 18:17:33 -0800

    Support connecting to another message bus server on the same port

--- io-bus.js
+++ io-bus.js
@@ -2,47 +2,66 @@
  * Created by Ramy Eldesoky on 8/14/2015.
  */
 var socket_io = require('socket.io');
+var socket_io_client = require("socket.io-client");
 var mb_server = require("./message_bus");
 var Promise = require("js-promise");
 var request_handlers = {};
 var subscribers = {};
 var client_version = "1.0.3";
-module.exports = function(param,express_app){
-	var io,server;
-	if(param == undefined){
-		param = 9666;
-	}
-	io = new socket_io(param,{serveClient:false});// start web sockets server
-	server = io.httpServer;
+var debug = require("debug")("io-bus");
 
-	if(express_app){
-		express_app.use("/io-bus/web-client.js",function(req,res,next){
-			serveClient(req,res);
-		});
-        express_app.use(require('connect-inject')({
-            snippet:'<script src="/io-bus/web-client.js"></script>'
-        }));
+module.exports = function(server,express_app){
+	var ret = new Promise();
+
+	if(server == undefined){
+		server = 9666;
 	}
-	else {
-		server.removeAllListeners('request');
-		server.on('error',function(msg){
+	var io, httpServer;
+
+	if(typeof server == "number"){
+		httpServer = require("http").Server(httpServerHandler);
+		httpServer.listen(server);
+		io = new socket_io(httpServer);
+		httpServer.on('error',function(msg){
 			if(msg.code == 'EADDRINUSE') {
 				console.log("Another server is running on the same port");
-				console.log("Server failure.. stopping");
+				debug("Server failure.. stopping");
+				// connect to that server
+				io = socket_io_client("http://localhost:" + server);
+				io.on("connect",function(){
+					debug("Successfully connected to the other server");
+					connectAsClient(io);
+				});
 			}
 		});
-		server.on("request", function (req, res) {
-			console.log("Request " + req.url);
-			if (req.url == "/io-bus/web-client.js") {
-				serveClient(req, res, server);//TODO: replace io(/*host address*/)
-			} else if (typeof param == "number") {
-				res.writeHead(404);
-				res.end();
-			}
-		});
+	}else{
+		io = new socket_io(server);// attach to existing server
+		if(express_app){
+			express_app.use("/io-bus/web-client.js",function(req,res,next){
+				serveClient(req,res);
+			});
+			express_app.use(require('connect-inject')({
+				snippet:'<script src="/io-bus/web-client.js"></script>'
+			}));
+		}else{
+			server.on("request",httpServerHandler);
+		}
 	}
 
-	function serveClient(req,res,server){
+	function httpServerHandler(req,res){
+		if (req.url == "/io-bus/web-client.js") {
+			debug("Requested " + req.url);
+			serveClient(req, res, "http://localhost:" + server);// pass url to pass to client connection
+		}else {
+			res.writeHead(404);
+			res.end();
+		}
+	}
+	//var io = new socket_io(url,{serveClient:false});// start web sockets server
+
+	httpServer = io.httpServer;
+
+	function serveClient(req,res,url){
 		var etag = req.headers['if-none-match'];
 		if (etag) {
 			if (client_version == etag) {
@@ -56,8 +75,8 @@ module.exports = function(param,express_app){
 		res.setHeader('ETag', client_version);
 		res.writeHead(200);
 		var webClient = read(require.resolve("./web-client.js"), 'utf-8');
-		if(server){//replace io(/*{host}*/) if not served by express app
-			webClient = webClient.replace("/*{host}*/",'"http://localhost:9666"');
+		if(url){//replace io(/*{host}*/) if not served by express app
+			webClient = webClient.replace("/*{host}*/",'"' + url + '"');
 		}
 		var scriptContent =read(require.resolve("./resources/socket.io.js"), 'utf-8') +
 			read(require.resolve("./resources/js-promise.js"), 'utf-8') +
@@ -66,10 +85,12 @@ module.exports = function(param,express_app){
 
 	}
 
-	io.on("connect",function(socket){// client connection
+	io.on("connect", connectAsHost);
+
+	function connectAsHost(socket){// client connection
 
 		// authorization should be added before
-		console.log("io: A client has connected to socket.io");
+		debug("io: A client has connected to socket.io");
 		var socket_owner, msg_bus;
 
 		socket.on("mb_connect",function(auth){// mb_connect should be { app_id:<app_id>, client_id:<client_id> }
@@ -78,15 +99,23 @@ module.exports = function(param,express_app){
 				return;
 			}
 			socket_owner = auth.client_id;
-			msg_bus = mb_server.connect(socket_owner);
-			console.log("io: Client (" + socket_owner + ") connected to Message Bus via:" + JSON.stringify(auth));
+			mb_server.connect(socket_owner,function(mb){
+				msg_bus = mb;
+			});
+			debug("io: Client (" + socket_owner + ") connected to Message Bus via:" + JSON.stringify(auth));
 
 			socket.on("mb_send",function(payload){// payload should be in the form {topic:"<topic>", msg:{},to:"<optional>"}
-				console.log("io: Received mb_send from(" + socket_owner + "), payload(" + JSON.stringify(payload) + ")");
+				debug("io: Received mb_send from(" + socket_owner + "), payload(" + JSON.stringify(payload) + ")");
+				if(!msg_bus) {
+					return;
+				}
 				msg_bus.send(payload.topic, payload.msg, payload.to);
 			});
 			socket.on("mb_request",function(request){// request should be in the form {topic:"<topic>", callback:"<unique_id>",query:{},to:"<optional>",timeout:<optional>}
-				console.log("io: Received mb_request from(" + socket_owner + "), request(" + JSON.stringify(request) + ")");
+				debug("io: Received mb_request from(" + socket_owner + "), request(" + JSON.stringify(request) + ")");
+				if(!msg_bus) {
+					return;
+				}
 				msg_bus.request(request.api, request.query, request.timeout, request.to).then(function(response){
 					response.topic = request.api + "_response";
 					socket.emit(request.callback || "mb_response", response);// response would be sent back to msg.callback
@@ -95,30 +124,40 @@ module.exports = function(param,express_app){
 				});
 			});
 			socket.on("mb_subscribe",function(payload){// payload should be {callback:"<unique_id>",topic:"<topic>",from:"<optional>"}
-				console.log( "io: Received mb_subscribe from(" + socket_owner + "), payload(" + JSON.stringify(payload) + ")" );
+				debug( "io: Received mb_subscribe from(" + socket_owner + "), payload(" + JSON.stringify(payload) + ")" );
+				if(!msg_bus) {
+					return;
+				}
 				subscribers[payload.callback] = msg_bus.on(payload.topic, function(msg){
 					socket.emit(payload.callback || "mb_broadcast", msg)
 				}, payload.from)
 			});
 			socket.on("mb_unsubscribe",function(payload){// payload should be {callback:"<unique_id>""}
+				if(!msg_bus) {
+					return;
+				}
 				msg_bus.off(subscribers[payload.callback]);
 			});
 			socket.on("mb_add_request_handler",function(request_handler){//request_handler format {api:<api>, callback:<unique_id>,limit_from:<optional>}
 
-				console.log( "io: IoRegRouter received mb_add_request_handler from(" + socket_owner + "), handler_info(" + JSON.stringify(request_handler) + ")" );
+				debug( "io: IoRegRouter received mb_add_request_handler from(" + socket_owner + "), handler_info(" + JSON.stringify(request_handler) + ")" );
+
+				if(!msg_bus) {
+					return;
+				}
 
 				request_handlers[request_handler.callback] = msg_bus.addRequestHandler(request_handler.api, function(query, from){//request format {topic:<topic>, query:{..},callback:<unique_id>}
-					console.log( "io: IoRegRouter received Request from(" + from + "), query(" + JSON.stringify(query) + ")" );
+					debug( "io: IoRegRouter received Request from(" + from + "), query(" + JSON.stringify(query) + ")" );
 					var callback = mb_server.uuid();
 					var ret = new Promise();
 					// wait for socket response
 					socket.once(callback, function (response) {
-						console.log( "io: IoReqRouter received a response from(" + socket_owner + "), payload(" + JSON.stringify(payload) + ")" );
+						debug( "io: IoReqRouter received a response from(" + socket_owner + "), payload(" + JSON.stringify(response) + ")" );
 						ret.resolve(response);
 					});
 					// send request to socket
 					var payload = {api:request_handler.api, query:query, from: from, callback: callback};
-					console.log( "io: IoReqRouter Emits request to (" + socket_owner + ":" + request_handler.callback + "), payload(" + JSON.stringify(payload) + ")" );
+					debug( "io: IoReqRouter Emits request to (" + socket_owner + ":" + request_handler.callback + "), payload(" + JSON.stringify(payload) + ")" );
 					socket.emit(request_handler.callback, payload);
 					return ret;
 
@@ -126,6 +165,9 @@ module.exports = function(param,express_app){
 			});
 
 			socket.on("mb_remove_request_handler",function(params){//params format {id:<socket_callback_uuid>}
+				if(!msg_bus) {
+					return;
+				}
 				msg_bus.off(request_handlers[params.id]);
 				delete(request_handlers[params.id]);
 			});
@@ -135,8 +177,117 @@ module.exports = function(param,express_app){
 			if(msg_bus && socket_owner){
 				msg_bus.disconnect();
 			}
-		})
-	});
+		});
 
-	return mb_server;
+		ret.resolve(mb_server);
+	}
+
+	function connectAsClient(socket){
+		var isConnected = true;
+
+		socket.on("disconnect",function(){
+			isConnected = false;
+		});
+
+		socket.on("connected",function(){
+			isConnected = true;
+		});
+
+		function create_uuid(){
+			return mb_server.uuid();
+		}
+
+		ret.resolve({
+			uuid:mb_server.uuid,
+
+			connect:function(my_id, callback){
+				if(!my_id){
+					my_id = this.uuid();
+				}
+
+				socket.emit("mb_connect",{client_id:my_id});
+
+				callback(			{
+					is_connected:function(){
+						return isConnected;
+					},
+					publish:function(topic, msg, to){
+						if(!isConnected){
+							return false;
+						}
+						socket.emit("mb_send",{topic:topic, msg:msg, to:to});
+						return true;
+					},
+					on:function(topic, callback, from, uuid){
+						if(!isConnected){
+							return false;
+						}
+						var callback_id = uuid || create_uuid();
+						socket.on(callback_id,function(msg){
+							callback(msg);
+						});
+						socket.emit("mb_subscribe",{topic:topic,callback: callback_id,from:from});
+						listeners[callback_id] = {
+							topic:topic,
+							callback:callback,
+							from:from
+						};
+						return callback_id;
+					},
+					once:function(topic, callback, from){
+						var self;
+						var uuid = this.on(topic,function(msg){
+							self.off(uuid);
+							callback(msg);
+						},from);
+					},
+					off:function(callback_id){
+						if(!callback_id || !isConnected){
+							return false;
+						}
+						socket.emit("mb_unsubscribe",{callback:callback_id});
+						socket.off(callback_id);
+						delete listeners[callback_id];
+						return true;
+					},
+					request:function(api, query, to){
+						if(!isConnected){
+							return Promise.error({disconnected:true});
+						}
+						var pr = new Promise();
+						var callback_id = create_uuid();
+						socket.on(callback_id,function(results){
+							if(results.error){
+								pr.reject(results.error)
+							}else {
+								pr.resolve(results);
+							}
+						});
+						socket.emit("mb_request",{api:api, query:query, callback:callback_id, to:to});
+						return pr;
+					},
+					addRequestHandler:function(api, callback, limit_from){
+						var callback_id = create_uuid();
+						socket.on(callback_id,function(payload){
+							var results = callback(payload.query,payload.from);
+							if(Promise.is(results)){
+								results.then(function(data){
+									socket.emit(payload.callback, data);// return the response
+								})
+							}else{
+								socket.emit(payload.callback, results);// return the response
+							}
+
+						});
+						socket.emit("mb_add_request_handler",{api:api,callback:callback_id,limit_from:limit_from});
+						return callback_id;
+					}
+				});
+
+			}
+		});
+	}
+
+	return ret;
+	//return mb_server;
 };
--- message_bus.js
+++ message_bus.js
@@ -2,6 +2,8 @@
  * Created by ramy on 8/13/2015.
  */
 var Promise = require("js-promise");
+var debug = require("debug")("msg-bus");
+
 var listeners = {
 // {topic}:{
 //      "{unique_handle}":{
@@ -29,7 +31,7 @@ function deliverMessage(msg){
 
 	function callListeners(event_listeners){
 		for( var handle in event_listeners ){
-			//console.log("Found Listener with handle=" + handle );
+			//debug("Found Listener with handle=" + handle );
 
 			if(!event_listeners.hasOwnProperty(handle)){
 				continue;
@@ -42,17 +44,17 @@ function deliverMessage(msg){
 				listeners_count ++;
 				//setTimeout(function(){
 				//	var listener_info = this;
-					console.log("mbus: Delivering a Msg to subscriber(" + listener_info.listener_id + "):" + JSON.stringify(msg) );
+					debug("Delivering a Msg to subscriber(" + listener_info.listener_id + "):" + JSON.stringify(msg) );
 					listener_info.callback({from: msg.from, topic:msg.topic, data:msg.data});
 				//}.bind(listener_info),1);
 			}else{
-				//console.log("Unqualified Message");
+				//debug("Unqualified Message");
 			}
 		}
 	}
-	//console.log("Check these listeners:" + JSON.stringify(listeners ));
+	//debug("Check these listeners:" + JSON.stringify(listeners ));
 	if(listeners[msg.topic]){
-		//console.log("Finding listeners for " + msg.topic + " message in " + JSON.stringify(listeners[msg.topic]) );
+		//debug("Finding listeners for " + msg.topic + " message in " + JSON.stringify(listeners[msg.topic]) );
 		callListeners(listeners[msg.topic]);
 	}
 
@@ -60,13 +62,13 @@ function deliverMessage(msg){
 		callListeners(listeners["*"]);
 	}
 
-	console.log("mbus: The msg: " + JSON.stringify(msg) + " has been delivered to (" + listeners_count + ") subscribers");
+	debug("The msg: " + JSON.stringify(msg) + " has been delivered to (" + listeners_count + ") subscribers");
 }
 
 var MsgBusManager = {
 	uuid:uuid,
 
-	connect:function(my_id){
+	connect:function(my_id, callback){
 		if(!my_id){
 			my_id = uuid();
 		}
@@ -135,7 +137,7 @@ var MsgBusManager = {
 				}catch(e){
 					info_str = data;
 				}
-				console.log("mbus: Publishing to topic(" + topic + "), content(" + info_str + ") to(" + (to ? to : " all") + ")");
+				debug("Publishing to topic(" + topic + "), content(" + info_str + ") to(" + (to ? to : " all") + ")");
 
 				setTimeout(function(){
 					deliverMessage({
@@ -151,7 +153,7 @@ var MsgBusManager = {
             },
 			request:function(api, params, timeout, to){
 				var callback = uuid();
-				console.log( "mbus: ReqRouter received request(" + api + "), with query(" + JSON.stringify(params) + ") from(" + my_id + ")");
+				debug( "ReqRouter received request(" + api + "), with query(" + JSON.stringify(params) + ") from(" + my_id + ")");
 				var retPromise = this.once(callback, timeout, to);
 
 				this.send("request",{
@@ -165,10 +167,10 @@ var MsgBusManager = {
 			},
 			//addRequestHandler callback should return data as a Promise to request caller
 			addRequestHandler:function(api, callback, limit_from/*optional*/){
-				console.log("mbus: ReqRouter registers request(" + api + ") handler for (" + (limit_from || "all" ) + ")" );
+				debug("ReqRouter registers request(" + api + ") handler for (" + (limit_from || "all" ) + ")" );
 				var self = this;
 				return this.on("request",function(request){
-					console.log("mbus: ReqRouter received request(" + request.api + "), data(" + JSON.stringify(request.data) + "), from(" + request.from + ")");
+					debug("ReqRouter received request(" + request.api + "), data(" + JSON.stringify(request.data) + "), from(" + request.from + ")");
 					if(request.data.api == api){
 						var response = callback(request.data.params,request.from);
 						Promise.as(response).then(function(return_val){
@@ -229,9 +231,11 @@ var MsgBusManager = {
 				//free all listeners and request handlers
 				this.off(usedHandles);
 			}
-		}
+		};
 
-        return MsgBusConnection;
+        return setTimeout(function(){
+	        callback(MsgBusConnection);
+        },1);
 	}
 };
 
